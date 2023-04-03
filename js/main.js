83be8f0f67cd0e14e286586e4e8fc37971381f6a
Абду-Муталиб <abkabylbekov97@gmail.com>
2023-04-03 20:09:32 +0500

    end of  project

--- js/main.js
+++ js/main.js
@@ -1,12 +1,35 @@
 $(function () {
-  var mixer = mixitup('.portfolio__content');
+  $(".menu a, .footer__top-link").on("click", function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body, html',).animate({scrollTop: top}, 1500);
+  });
+
+
+
   Fancybox.bind("[data-fancybox]", {
     // Your custom options
   });
-
+  
   $('.slider-blog__inner').slick({
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="./image/arrow-left.svg" alt="arrow-left"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="./image/arrow-right.svg" alt="arrow-right"></button>',
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false
+        }
+      },
+    ]
   })
+  
+  $('.menu__btn, .menu a').on('click', function(){
+    $('.header__top-inner').toggleClass('header__top-inner--active');
+  });
+
+
+  var mixer = mixitup('.portfolio__content');
 });
